9bba3cbaa37bf1442d138820afddce6d98f70d7c
Your Name <zhanerke02nurzhan.com>
2020-11-14 02:25:45 +0600

    calculator

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -5,25 +5,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    Button btnClicked;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText number1, number2;
+    Button btnPlus;
+    Button btnMinus;
+    Button btnMulti;
+    Button btnDivide;
+    TextView getResults;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.button_on_click);
+        setContentView(R.layout.calculator);
 
-        textView = findViewById(R.id.tvText);
-        btnClicked = findViewById(R.id.btn);
+        number1 = findViewById(R.id.edit_number1);
+        number2 = findViewById(R.id.edit_number2);
+
+        btnPlus = findViewById(R.id.btn_plus);
+        btnMinus = findViewById(R.id.btn_minus);
+        btnMulti = findViewById(R.id.btn_multi);
+        btnDivide = findViewById(R.id.btn_divide);
+
+        getResults = findViewById(R.id.textView);
+
+        btnPlus.setOnClickListener(this);
+        btnMinus.setOnClickListener(this);
+        btnDivide.setOnClickListener(this);
+        btnMulti.setOnClickListener(this);
+
+
     }
+    @Override
+    public void onClick(View view){
+        int res = 0;
 
-    public void btn(View view) {
-        textView.setText("Button Is Clicked");
+        if(view.getId() == R.id.btn_plus){
+            res = Integer.parseInt(number1.getText().toString())
+                    + Integer.parseInt(number2.getText().toString());
+        }
+        else if(view.getId() == R.id.btn_minus){
+            res = Integer.parseInt(number1.getText().toString())
+                    - Integer.parseInt(number2.getText().toString());
+        }
+        else if(view.getId() == R.id.btn_multi){
+            res = Integer.parseInt(number1.getText().toString())
+                    * Integer.parseInt(number2.getText().toString());
+        }
+        else{
+            res = Integer.parseInt(number1.getText().toString())
+                    / Integer.parseInt(number2.getText().toString());
+
+        }
+
+        getResults.setText("Result: " + res);
     }
 }
