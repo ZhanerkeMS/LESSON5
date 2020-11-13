187699b50d51ebc530a5bbdd500542862155ef7c
Your Name <zhanerke02nurzhan.com>
2020-11-13 22:34:32 +0600

    uludshaem registracionnuyu

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button btnClicked;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.button_on_click);
+
+        textView = findViewById(R.id.tvText);
+        btnClicked = findViewById(R.id.btn);
+    }
+
+    public void btn(View view) {
+        textView.setText("Button Is Clicked");
     }
 }
